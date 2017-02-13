28f8e4c7853d9675fd89d68e2aeddd07526033eb
jannemikkonen <janne.mikkonen_94@hotmail.fi>
2017-02-13 10:10:04 +0200

    Suomi on uusi mailmanmestari

--- src/main/java/testi/Hello.java
+++ src/main/java/testi/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("hello Suomi 100v, Vaasa, Palosaari!");
+		System.out.println("Suomi on uusi mailmanmestari");
 	}
 }
