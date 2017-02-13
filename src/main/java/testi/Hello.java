a270ba42f552eafb94e01fa514e165d3adb91f10
jannemikkonen <janne.mikkonen_94@hotmail.fi>
2017-02-13 10:29:21 +0200

    Update Hello.java
    wolffintie

--- src/main/java/testi/Hello.java
+++ src/main/java/testi/Hello.java
@@ -3,7 +3,7 @@ package testi;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("hello Suomi 100v, Vaasa, Palosaari!");
+		System.out.println("hello Suomi 100v, Vaasa, Palosaari, Wolffintie!");
 		System.out.println("Suomi on uusi mailmanmestari");
 	}
 }
